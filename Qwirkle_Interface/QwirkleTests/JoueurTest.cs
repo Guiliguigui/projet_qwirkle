ace8e55fd41f43af32a1aef339ba39f9221e9c88
Guillaume <gui.mairesse@gmail.com>
2019-05-07 19:08:44 +0200

    Début de l'implémentation des tests et des classes et ajout des images
    Classe Joueur Finie, Classe Pioche à Revoir (ajouter une fonction pour compter les tuiles totales de la pioche) et Classe Tuile Non commencée.

---
+++ Qwirkle_Interface/QwirkleTests/JoueurTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QwirkleLibrary;
+
+namespace QwirkleTests
+{
+    [TestClass]
+    public class JoueurTest
+    {
+        [TestMethod]
+        public void TestJoueurConstructor()
+        {
+            Joueur Joueur_1 = new Joueur("Louis", 0, 1);
+            
+            Assert.AreEqual("Louis", Joueur_1.Get_name());
+            Assert.AreEqual(0, Joueur_1.Get_points());
+            Assert.AreEqual(1, Joueur_1.Get_numero());
+        }
+    }
+}
---
+++ Qwirkle_Interface/QwirkleTests/PiocheTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QwirkleLibrary;
+
+namespace QwirkleTests
+{
+    [TestClass]
+    public class PiocheTest
+    {
+        [TestMethod]
+        public void TestPiocheConstructor()
+        {
+            Pioche LaPioche = new Pioche(int[,] tab_TuilesRestantes);
+
+            Assert.AreEqual({ { 3,3,3,3,3,3},{ 3,3,3,3,3,3},{ 3,3,3,3,3,3},{ 3,3,3,3,3,3},{ 3,3,3,3,3,3},{ 3,3,3,3,3,3}}, Lapioche.tab_TuilesRestantes);
+
+        }
+    }
+}
