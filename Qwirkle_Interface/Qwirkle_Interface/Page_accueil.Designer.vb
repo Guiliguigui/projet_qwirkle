3394ca0e528e73620fd51af3ccb059e4a4e7b502
HF21 <hugo.fer@etud.u-picardie.fr>
2019-05-21 11:19:31 +0200

    petite amélioration interface

--- Qwirkle_Interface/Qwirkle_Interface/Page_accueil.Designer.vb
+++ Qwirkle_Interface/Qwirkle_Interface/Page_accueil.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Page_accueil
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Page_accueil))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
@@ -54,22 +55,21 @@ Partial Class Page_accueil
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(296, 11)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Image = CType(resources.GetObject("Label1.Image"), System.Drawing.Image)
+        Me.Label1.Location = New System.Drawing.Point(222, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(112, 31)
+        Me.Label1.Size = New System.Drawing.Size(96, 25)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Qwirkle"
+        Me.Label1.Text = "            "
         '
         'Panel1
         '
         Me.Panel1.Controls.Add(Me.RadioButton1)
         Me.Panel1.Controls.Add(Me.RadioButton2)
         Me.Panel1.Controls.Add(Me.RadioButton3)
-        Me.Panel1.Location = New System.Drawing.Point(16, 46)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel1.Location = New System.Drawing.Point(12, 37)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(685, 49)
+        Me.Panel1.Size = New System.Drawing.Size(514, 40)
         Me.Panel1.TabIndex = 1
         '
         'RadioButton1
@@ -77,9 +77,10 @@ Partial Class Page_accueil
         Me.RadioButton1.Appearance = System.Windows.Forms.Appearance.Button
         Me.RadioButton1.Checked = True
         Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RadioButton1.Location = New System.Drawing.Point(32, 6)
+        Me.RadioButton1.Location = New System.Drawing.Point(24, 5)
+        Me.RadioButton1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(175, 40)
+        Me.RadioButton1.Size = New System.Drawing.Size(131, 32)
         Me.RadioButton1.TabIndex = 4
         Me.RadioButton1.TabStop = True
         Me.RadioButton1.Text = "2 Joueurs"
@@ -90,9 +91,10 @@ Partial Class Page_accueil
         '
         Me.RadioButton2.Appearance = System.Windows.Forms.Appearance.Button
         Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RadioButton2.Location = New System.Drawing.Point(253, 6)
+        Me.RadioButton2.Location = New System.Drawing.Point(190, 5)
+        Me.RadioButton2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(175, 40)
+        Me.RadioButton2.Size = New System.Drawing.Size(131, 32)
         Me.RadioButton2.TabIndex = 3
         Me.RadioButton2.Text = "3 Joueurs"
         Me.RadioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -102,9 +104,10 @@ Partial Class Page_accueil
         '
         Me.RadioButton3.Appearance = System.Windows.Forms.Appearance.Button
         Me.RadioButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RadioButton3.Location = New System.Drawing.Point(473, 6)
+        Me.RadioButton3.Location = New System.Drawing.Point(355, 5)
+        Me.RadioButton3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(175, 40)
+        Me.RadioButton3.Size = New System.Drawing.Size(131, 32)
         Me.RadioButton3.TabIndex = 2
         Me.RadioButton3.Text = "4 Joueurs"
         Me.RadioButton3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -117,11 +120,9 @@ Partial Class Page_accueil
         Me.GroupBox1.Controls.Add(Me.Panelj1)
         Me.GroupBox1.Controls.Add(Me.Panelj4)
         Me.GroupBox1.Controls.Add(Me.Panelj2)
-        Me.GroupBox1.Location = New System.Drawing.Point(16, 118)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 96)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
-        Me.GroupBox1.Size = New System.Drawing.Size(685, 254)
+        Me.GroupBox1.Size = New System.Drawing.Size(514, 206)
         Me.GroupBox1.TabIndex = 2
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Noms des joueurs"
@@ -130,10 +131,9 @@ Partial Class Page_accueil
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(32, 220)
-        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Location = New System.Drawing.Point(24, 179)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(599, 20)
+        Me.Label6.Size = New System.Drawing.Size(471, 16)
         Me.Label6.TabIndex = 4
         Me.Label6.Text = "Les joueurs saisissent leurs noms dans l'ordre dans lequel ils souhaitent jouer."
         '
@@ -142,27 +142,24 @@ Partial Class Page_accueil
         Me.Panelj3.Controls.Add(Me.TextBoxJ3)
         Me.Panelj3.Controls.Add(Me.Label4)
         Me.Panelj3.Enabled = False
-        Me.Panelj3.Location = New System.Drawing.Point(32, 124)
-        Me.Panelj3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panelj3.Location = New System.Drawing.Point(24, 101)
         Me.Panelj3.Name = "Panelj3"
-        Me.Panelj3.Size = New System.Drawing.Size(299, 87)
+        Me.Panelj3.Size = New System.Drawing.Size(224, 71)
         Me.Panelj3.TabIndex = 3
         '
         'TextBoxJ3
         '
-        Me.TextBoxJ3.Location = New System.Drawing.Point(56, 38)
-        Me.TextBoxJ3.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxJ3.Location = New System.Drawing.Point(42, 31)
         Me.TextBoxJ3.Name = "TextBoxJ3"
-        Me.TextBoxJ3.Size = New System.Drawing.Size(188, 22)
+        Me.TextBoxJ3.Size = New System.Drawing.Size(142, 20)
         Me.TextBoxJ3.TabIndex = 1
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(99, 18)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Location = New System.Drawing.Point(74, 15)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(113, 17)
+        Me.Label4.Size = New System.Drawing.Size(85, 13)
         Me.Label4.TabIndex = 0
         Me.Label4.Text = "Nom du joueur 3"
         '
@@ -170,27 +167,24 @@ Partial Class Page_accueil
         '
         Me.Panelj1.Controls.Add(Me.TextBoxJ1)
         Me.Panelj1.Controls.Add(Me.Label2)
-        Me.Panelj1.Location = New System.Drawing.Point(32, 23)
-        Me.Panelj1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panelj1.Location = New System.Drawing.Point(24, 19)
         Me.Panelj1.Name = "Panelj1"
-        Me.Panelj1.Size = New System.Drawing.Size(299, 84)
+        Me.Panelj1.Size = New System.Drawing.Size(224, 68)
         Me.Panelj1.TabIndex = 3
         '
         'TextBoxJ1
         '
-        Me.TextBoxJ1.Location = New System.Drawing.Point(56, 38)
-        Me.TextBoxJ1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxJ1.Location = New System.Drawing.Point(42, 31)
         Me.TextBoxJ1.Name = "TextBoxJ1"
-        Me.TextBoxJ1.Size = New System.Drawing.Size(188, 22)
+        Me.TextBoxJ1.Size = New System.Drawing.Size(142, 20)
         Me.TextBoxJ1.TabIndex = 1
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(99, 18)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Location = New System.Drawing.Point(74, 15)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(113, 17)
+        Me.Label2.Size = New System.Drawing.Size(85, 13)
         Me.Label2.TabIndex = 0
         Me.Label2.Text = "Nom du joueur 1"
         '
@@ -199,27 +193,24 @@ Partial Class Page_accueil
         Me.Panelj4.Controls.Add(Me.TextBoxJ4)
         Me.Panelj4.Controls.Add(Me.Label5)
         Me.Panelj4.Enabled = False
-        Me.Panelj4.Location = New System.Drawing.Point(349, 124)
-        Me.Panelj4.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panelj4.Location = New System.Drawing.Point(262, 101)
         Me.Panelj4.Name = "Panelj4"
-        Me.Panelj4.Size = New System.Drawing.Size(299, 87)
+        Me.Panelj4.Size = New System.Drawing.Size(224, 71)
         Me.Panelj4.TabIndex = 3
         '
         'TextBoxJ4
         '
-        Me.TextBoxJ4.Location = New System.Drawing.Point(57, 38)
-        Me.TextBoxJ4.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxJ4.Location = New System.Drawing.Point(43, 31)
         Me.TextBoxJ4.Name = "TextBoxJ4"
-        Me.TextBoxJ4.Size = New System.Drawing.Size(188, 22)
+        Me.TextBoxJ4.Size = New System.Drawing.Size(142, 20)
         Me.TextBoxJ4.TabIndex = 1
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(95, 18)
-        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Location = New System.Drawing.Point(71, 15)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(113, 17)
+        Me.Label5.Size = New System.Drawing.Size(85, 13)
         Me.Label5.TabIndex = 0
         Me.Label5.Text = "Nom du joueur 4"
         '
@@ -227,51 +218,46 @@ Partial Class Page_accueil
         '
         Me.Panelj2.Controls.Add(Me.TextBoxJ2)
         Me.Panelj2.Controls.Add(Me.Label3)
-        Me.Panelj2.Location = New System.Drawing.Point(349, 23)
-        Me.Panelj2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panelj2.Location = New System.Drawing.Point(262, 19)
         Me.Panelj2.Name = "Panelj2"
-        Me.Panelj2.Size = New System.Drawing.Size(299, 84)
+        Me.Panelj2.Size = New System.Drawing.Size(224, 68)
         Me.Panelj2.TabIndex = 2
         '
         'TextBoxJ2
         '
-        Me.TextBoxJ2.Location = New System.Drawing.Point(57, 39)
-        Me.TextBoxJ2.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxJ2.Location = New System.Drawing.Point(43, 32)
         Me.TextBoxJ2.Name = "TextBoxJ2"
-        Me.TextBoxJ2.Size = New System.Drawing.Size(188, 22)
+        Me.TextBoxJ2.Size = New System.Drawing.Size(142, 20)
         Me.TextBoxJ2.TabIndex = 1
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(95, 20)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Location = New System.Drawing.Point(71, 16)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(113, 17)
+        Me.Label3.Size = New System.Drawing.Size(85, 13)
         Me.Label3.TabIndex = 0
         Me.Label3.Text = "Nom du joueur 2"
         '
         'commencer
         '
         Me.commencer.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.commencer.Location = New System.Drawing.Point(16, 379)
-        Me.commencer.Margin = New System.Windows.Forms.Padding(4)
+        Me.commencer.Location = New System.Drawing.Point(12, 308)
         Me.commencer.Name = "commencer"
-        Me.commencer.Size = New System.Drawing.Size(685, 63)
+        Me.commencer.Size = New System.Drawing.Size(514, 51)
         Me.commencer.TabIndex = 3
         Me.commencer.Text = "Commencer à jouer"
         Me.commencer.UseVisualStyleBackColor = True
         '
         'Page_accueil
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(723, 455)
+        Me.ClientSize = New System.Drawing.Size(542, 370)
         Me.Controls.Add(Me.commencer)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Page_accueil"
         Me.Text = "Qwirkle"
         Me.Panel1.ResumeLayout(False)
