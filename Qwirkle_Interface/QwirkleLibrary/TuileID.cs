b9c263444b707031ea95f9f8d6ae5df603ada29f
idris <idris.rahmani@etud.u-picardie.fr>
2019-05-23 12:23:49 +0200

    classe TuileID

---
+++ Qwirkle_Interface/QwirkleLibrary/TuileID.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QwirkleLibrary
+{
+    class TuileID
+    {
+        public int TuileValue(string tuile)
+        {
+            int Forme=1, Couleur=1;
+            switch(tuile)
+            {
+                case "Trefle" :
+                    Forme = 1;
+                    break;
+
+            }
+            return Forme+Couleur;
+        }
+    }
+}
